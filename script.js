7c6f381114a98388d0bff6dbb1a15b8617f8611c
BRAITOU55555 <albertsimah5@gmail.com>
2024-11-02 19:35:06 +0100

    Ajustements CSS pour l'image et alignement de la section 'Notre Processus'

---
+++ script.js
@@ -0,0 +1,5 @@
+// Fonction pour activer/désactiver le menu mobile
+function toggleMenu() {
+    const navbarMobile = document.querySelector('.navbar-mobile');
+    navbarMobile.classList.toggle('active');
+}
